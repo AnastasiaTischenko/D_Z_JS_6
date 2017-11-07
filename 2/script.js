7631a98f825e74715891bae21e9724871a6134f6
Nastya Tischenko <atisenko93@gmail.com>
2017-11-07 17:01:34 +0200

    second comm

--- 2/script.js
+++ 2/script.js
@@ -1,4 +1,4 @@
 document.querySelector('.cl-op').ondblclick = function(){
     var m = document.querySelector('.cl-op');
-    m.style.backgroundPosition = '162px 0';
+    m.classList.toggle('toggleClass');
 }
--- 3/js.js
+++ 3/js.js
@@ -4,18 +4,20 @@ function f1(event){
         b.innerHTML += '<div class="block"></div>';
     }
     var rad = document.querySelectorAll('.block');
-    for (let i = 0; i < 100; i++){
+    for (var i = 0; i < 100; i++){
         rad[i].onmousemove = function(event){
-            console.log(event.target.className)
             if (event.target.className == 'block'){
                 changeRad(event.target);
             }
         }
+        rad[i].onmouseleave = function(event){
+                changeRad(event.target);
+            
+        }
     }
     
     function changeRad(e){
-        e.style.borderRadius = '50%';
-        e.style.backgroundColor = 'yellow';
+        e.classList.toggle('yellowRound');
     }
 
 };
